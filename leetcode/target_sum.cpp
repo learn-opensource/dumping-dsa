64876c353287dea33becd67c06fcea94948bf5d5
Sachin Duhan <38427564+sachin-duhan26@users.noreply.github.com>
2022-10-30 15:26:29 +0530

    dumpester

---
+++ leetcode/target_sum.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// complete the function that return weather a given target sum exist in an array or not!
+bool doesTargetSumExist(vector<int> arr, int target){
+    // complete the code.
+}
+
+int main()
+{
+
+    return 0;
+}
