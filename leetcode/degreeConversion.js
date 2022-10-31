fde6d189bc9dad0adc79dbca77e2b425ae5500ff
tanu kumari <tanukumarisinha.123@gmail.com>
2022-10-31 18:47:59 +0530

    degreeConversion

---
+++ leetcode/degreeConversion.js
@@ -0,0 +1,8 @@
+// program to convert celsius to fahrenheit
+const celsius = prompt("Enter a celsius value: ");
+
+// calculate fahrenheit
+const fahrenheit = (celsius * 1.8) + 32
+
+// display the result
+console.log(`${celsius} degree celsius is equal to ${fahrenheit} degree fahrenheit.`);
